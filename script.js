3bdf44388c3376e49c5158234d360f324a572915
nithinbairu <113778598+nithinbairu@users.noreply.github.com>
2025-09-28 23:27:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 const makeChange = (c) => {
   // your name here
+	obj={
+		"q":25,
+		"d":10,
+		"n":5,
+		"p":1
+	}
+	let keys=Object.keys(obj);
+	let ans={
+		"q":0,
+		"d":0,
+		"n":0,
+		"p":0
+	}
+	
+	for(let key of keys){
+		while(c>=obj[key]){
+			ans[key]+=1;
+			c-=obj[key];
+		}
+	}
+	return ans;
 };
-
 // Do not the change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
